afb60f9555f06a8b60c571fd3c5a8ecaeaba3214
BiDuang <2267304497@qq.com>
2022-11-03 21:08:33 +0800

    Rev - 1.4.39

---
+++ Model/About.py
@@ -0,0 +1,19 @@
+from PySide6.QtWidgets import *
+from PySide6.QtUiTools import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+
+
+class About(QMainWindow):
+
+    __author__ = "BiDuang"
+    __version__ = "Rev - 1.4.39"
+    def __init__(self):
+        super().__init__()
+        self.ui = QUiLoader().load("View\\About.ui")
+        self.ui.setWindowFlags(
+            Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint)
+        self.ui.setFixedSize(self.ui.width(), self.ui.height())
+        self.ui.version.setText(self.__version__)
+
+    
---
+++ main.py
@@ -0,0 +1,21 @@
+from View.src.logo import *
+from Model import MainWindow
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtUiTools import *
+
+app = QApplication([])
+BootMask = QUiLoader().load("View\\Boot.ui")
+BootMask.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint |
+                        Qt.WindowType.FramelessWindowHint)
+BootMask.show()
+delayTime = 1
+timer = QtCore.QElapsedTimer()
+timer.start()
+while (timer.elapsed() < (delayTime * 1000)):
+    app.processEvents()
+    BootMask.progressBar.setValue((timer.elapsed()/delayTime * 1000)/10000)
+BootMask.close()
+window = MainWindow.MainWindow()
+window.ui.show()
+app.exec()
